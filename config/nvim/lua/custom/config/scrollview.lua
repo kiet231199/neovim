5c97046852d20383589f5b3d16f8c2d41812ca4a
kiet231199 <phamanhkiet231199@gmail.com>
2023-06-05 21:17:40 +0700

    Config: Update scrollview

--- config/nvim/lua/custom/config/scrollview.lua
+++ config/nvim/lua/custom/config/scrollview.lua
@@ -5,7 +5,6 @@ if not status_ok then
 end
 
 scrollview.setup({
-	mode = 'virtual',
 	zindex = 30,
 	winblend = 50,
 	base = 'right',
@@ -13,7 +12,7 @@ scrollview.setup({
 	current_only = true,
 	hide_on_intersect = false,
 	line_limit = 5000,
-	signs_max_per_row = 0,
+	signs_max_per_row = 1,
 	signs_column = 1,
 	signs_on_startup = {'search', 'diagnostics', 'folds' },
 	signs_show_in_folds = true,
@@ -32,65 +31,3 @@ scrollview.setup({
 	search_symbol = '',
 })
 
--- Add config with gitsigns
-local group = 'gitsigns'
-local add = scrollview.register_sign_spec({
-	group = group,
-	highlight = 'GitSignsAdd',
-	symbol = ' ',
-}).name
-local change = scrollview.register_sign_spec({
-	group = group,
-	highlight = 'GitSignsChange',
-	symbol = ' ',
-}).name
-local delete = scrollview.register_sign_spec({
-	group = group,
-	highlight = 'GitSignsDelete',
-	symbol = ' ',
-}).name
-
-scrollview.set_sign_group_state(group, enable)
-
-vim.api.nvim_create_autocmd('User', {
-	pattern = 'ScrollViewRefresh',
-	callback = function()
-		if not scrollview.is_sign_group_active(group) then return end
-		local success, gitsigns = pcall(require, 'gitsigns')
-		if not success then return end
-		for _, winid in ipairs(scrollview.get_sign_eligible_windows()) do
-			local bufnr = vim.api.nvim_win_get_buf(winid)
-			local hunks = gitsigns.get_hunks(bufnr) or {}
-			local lines_add = {}
-			local lines_change = {}
-			local lines_delete = {}
-			for _, hunk in ipairs(hunks) do
-				if hunk.type == 'add' then
-					-- Don't show if the entire column would be covered.
-					if hunk.added.count < vim.api.nvim_buf_line_count(bufnr) then
-						for line = hunk.added.start, hunk.added.start + hunk.added.count - 1 do
-							table.insert(lines_add, line)
-						end
-					end
-				elseif hunk.type == 'change' then
-					for line = hunk.added.start, hunk.added.start + hunk.added.count - 1 do
-						table.insert(lines_change, line)
-					end
-				elseif hunk.type == 'delete' then
-					table.insert(lines_delete, hunk.added.start)
-				end
-			end
-			vim.b[bufnr][add] = lines_add
-			vim.b[bufnr][change] = lines_change
-			vim.b[bufnr][delete] = lines_delete
-		end
-	end
-})
-
-vim.api.nvim_create_autocmd('User', {
-	pattern = 'GitSignsUpdate',
-	callback = function()
-		if not scrollview.is_sign_group_active(group) then return end
-		vim.cmd('ScrollViewRefresh')
-	end
-})
