5066848a82890eebbdf4cded332d88a79f50da9f
kiet231199 <phamanhkiet231199@gmail.com>
2023-09-05 01:09:31 +0700

    Config: Update winsep

--- config/nvim/lua/custom/config/winsep.lua
+++ config/nvim/lua/custom/config/winsep.lua
@@ -20,13 +20,13 @@ winsep.setup({
 		-- Executed after closing the window divider
 	end,
 	create_event = function()
-		-- local win_n = require("colorful-winsep.utils").calculate_number_windows()
-		-- if win_n == 2 then
-		-- 	local win_id = vim.fn.win_getid(vim.fn.winnr('h'))
-		-- 	local filetype = vim.api.nvim_get_option_value('filetype', { buf = vim.api.nvim_win_get_buf(win_id) })
-		-- 	if filetype == "neo-tree" then
-		-- 		winsep.NvimSeparatorDel()
-		-- 	end
-		-- end
+		local win_n = require("colorful-winsep.utils").calculate_number_windows()
+		if win_n == 2 then
+			local win_id = vim.fn.win_getid(vim.fn.winnr('h'))
+			local filetype = vim.api.nvim_get_option_value('filetype', { buf = vim.api.nvim_win_get_buf(win_id) })
+			if filetype == "neo-tree" then
+				winsep.NvimSeparatorDel()
+			end
+		end
 	end
 })
