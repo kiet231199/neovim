c87a638f5b6446154f22b5dbee76c736256afcbb
kiet231199 <phamanhkiet231199@gmail.com>
2023-09-06 17:16:37 +0700

    Config: Update heirline - try to use tabline

--- config/nvim/lua/custom/config/heirline.lua
+++ config/nvim/lua/custom/config/heirline.lua
@@ -251,6 +251,13 @@ local has_git = {
         end,
         provider = ' ',
     },
+    on_click = {
+        callback = function()
+            vim.cmd("Gitsigns toggle_current_line_blame")
+        end,
+        update = true,
+        name = "toggle_git_blame",
+    },
 }
 
 local none_git = {
@@ -434,6 +441,12 @@ local diagnostics = {
             hl = { fg = get_hex("DiagnosticHint").fg },
         },
         { provider = " " },
+    },
+    on_click = {
+        callback = function()
+            require("trouble").toggle({ mode = "document_diagnostics" })
+        end,
+        name = "open_trouble",
     },
 }
 
@@ -651,9 +664,226 @@ local statusline = {
     },
 }
 
+local TablineFileNameBlock = {
+    init = function(self)
+        self.filename = vim.api.nvim_buf_get_name(self.bufnr)
+    end,
+    hl = function(self)
+        if self.is_active then
+            return "TabLineSel"
+        -- why not?
+        -- elseif not vim.api.nvim_buf_is_loaded(self.bufnr) then
+        --     return { fg = "gray" }
+        else
+            return "TabLine"
+        end
+    end,
+    {
+        provider = function(self)
+            return tostring(self.bufnr) .. ". "
+        end,
+        hl = "Comment",
+    },
+    {
+        init = function(self)
+            local filename = self.filename
+            local extension = vim.fn.fnamemodify(filename, ":e")
+            self.icon, self.icon_color = require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
+        end,
+        provider = function(self)
+            return self.icon and (self.icon .. " ")
+        end,
+        hl = function(self)
+            return { fg = self.icon_color }
+        end
+    },
+    {
+        provider = function(self)
+            -- self.filename will be defined later, just keep looking at the example!
+            local filename = self.filename
+            filename = filename == "" and "[No Name]" or vim.fn.fnamemodify(filename, ":t")
+            return filename
+        end,
+        hl = function(self)
+            return { bold = self.is_active or self.is_visible, italic = true }
+        end,
+    },
+    {
+        {
+            condition = function(self)
+                return vim.api.nvim_buf_get_option(self.bufnr, "modified")
+            end,
+            provider = "[+]",
+            hl = { fg = "green" },
+        },
+        {
+            condition = function(self)
+                return not vim.api.nvim_buf_get_option(self.bufnr, "modifiable")
+                    or vim.api.nvim_buf_get_option(self.bufnr, "readonly")
+            end,
+            provider = function(self)
+                if vim.api.nvim_buf_get_option(self.bufnr, "buftype") == "terminal" then
+                    return "  "
+                else
+                    return ""
+                end
+            end,
+            hl = { fg = "orange" },
+        },
+    },
+    {
+        condition = function(self)
+            return not vim.api.nvim_buf_get_option(self.bufnr, "modified")
+        end,
+        { provider = " " },
+        {
+            provider = "",
+            hl = { fg = "gray" },
+            on_click = {
+                callback = function(_, minwid)
+                    vim.schedule(function()
+                        vim.api.nvim_buf_delete(minwid, { force = false })
+                        vim.cmd.redrawtabline()
+                    end)
+                end,
+                minwid = function(self)
+                    return self.bufnr
+                end,
+                name = "heirline_tabline_close_buffer_callback",
+            },
+        },
+    },
+    on_click = {
+        callback = function(_, minwid, _, button)
+            if (button == "m") then -- close on mouse middle click
+                vim.schedule(function()
+                    vim.api.nvim_buf_delete(minwid, { force = false })
+                end)
+            else
+                vim.api.nvim_win_set_buf(0, minwid)
+            end
+        end,
+        minwid = function(self)
+            return self.bufnr
+        end,
+        name = "heirline_tabline_buffer_callback",
+    },
+}
+
+local TablineBufferBlock = utils.surround({ "", " " }, function(self)
+    if self.is_active then
+        return utils.get_highlight("TabLineSel").bg
+    else
+        return utils.get_highlight("TabLine").bg
+    end
+end, { TablineFileNameBlock })
+
+-- this is the default function used to retrieve buffers
+local get_bufs = function()
+    return vim.tbl_filter(function(bufnr)
+        return vim.api.nvim_buf_get_option(bufnr, "buflisted")
+    end, vim.api.nvim_list_bufs())
+end
+
+-- initialize the buflist cache
+local buflist_cache = {}
+
+-- setup an autocmd that updates the buflist_cache every time that buffers are added/removed
+vim.api.nvim_create_autocmd({ "VimEnter", "UIEnter", "BufAdd", "BufDelete" }, {
+    callback = function()
+        vim.schedule(function()
+            local buffers = get_bufs()
+            for i, v in ipairs(buffers) do
+                buflist_cache[i] = v
+            end
+            for i = #buffers + 1, #buflist_cache do
+                buflist_cache[i] = nil
+            end
+        end)
+    end,
+})
+
+local BufferLine = utils.make_buflist(
+    TablineBufferBlock,
+    { provider = " ", hl = { fg = "gray" } },
+    { provider = " ", hl = { fg = "gray" } },
+    -- out buf_func simply returns the buflist_cache
+    function()
+        print(#buflist_cache)
+        return buflist_cache
+    end,
+    -- no cache, as we're handling everything ourselves
+    false
+)
+
+local TabLineOffset = {
+    condition = function(self)
+        local win = vim.api.nvim_tabpage_list_wins(0)[1]
+        local bufnr = vim.api.nvim_win_get_buf(win)
+        self.winid = win
+
+        if vim.bo[bufnr].filetype == "neo-tree" then
+            self.title = "Neo-tree"
+            return true
+        -- elseif vim.bo[bufnr].filetype == "TagBar" then
+        --     ...
+        end
+    end,
+
+    provider = function(self)
+        local title = self.title
+        local width = vim.api.nvim_win_get_width(self.winid)
+        local pad = math.ceil((width - #title) / 2)
+        return string.rep(" ", pad) .. title .. string.rep(" ", pad)
+    end,
+
+    hl = function(self)
+        if vim.api.nvim_get_current_win() == self.winid then
+            return "TablineSel"
+        else
+            return "Tabline"
+        end
+    end,
+}
+
+local Tabpage = {
+    provider = function(self)
+        return "%" .. self.tabnr .. "T " .. self.tabpage .. " %T"
+    end,
+    hl = function(self)
+        if not self.is_active then
+            return "TabLine"
+        else
+            return "TabLineSel"
+        end
+    end,
+}
+
+local TabPages = {
+    -- only show this component if there's 2 or more tabpages
+    condition = function()
+        return #vim.api.nvim_list_tabpages() >= 2
+    end,
+    { provider = "%=" },
+    utils.make_tablist(Tabpage),
+    {
+        provider = "%999X  %X",
+        hl = "TabLine",
+    },
+}
+
+local TabLine = {
+    TabLineOffset,
+    BufferLine,
+    TabPages
+}
+
 heirline.setup({
     statusline = statusline,
+    -- tabline = TabLine,
     opts = {
         disable_winbar_cb = true,
     },
 })
+vim.opt.showtabline = 2
+vim.cmd([[au FileType * if index(['wipe', 'delete'], &bufhidden) >= 0 | set nobuflisted | endif]])
