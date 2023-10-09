fc4e303d6f0abc844711e748a8ff9cb38faa31d7
kiet231199 <phamanhkiet231199@gmail.com>
2023-10-09 22:38:17 +0700

    Config: Custom: Update align v2

--- config/nvim-custom/lua/plugin/editor/align.lua
+++ config/nvim-custom/lua/plugin/editor/align.lua
@@ -1,17 +1,36 @@
--- align_to_char(length, reverse, preview, marks)
--- align_to_string(is_pattern, reverse, preview, marks)
--- align(str, reverse, marks)
--- operator(fn, opts)
--- Where:
---      length: integer
---      reverse: boolean
---      preview: boolean
---      marks: table (e.g. {1, 0, 23, 15})
---      str: string (can be plaintext or Lua pattern if is_pattern is true)
+vim.keymap.set('x', '<space>ac', function()
+    require'align'.align_to_char({
+        length = 1,
+    })
+end, { desc = "Align to 1 char" }) -- Aligns to 1 character, looking left
 
-local NS = { noremap = true, silent = true }
+vim.keymap.set('x', '<space>aC', function()
+    require'align'.align_to_char({
+        preview = true,
+        length = 2,
+    })
+end, { desc = "Align to 2 char" }) -- Aligns to 2 characters, looking left and with previews
 
-vim.keymap.set('x', '<space>ac', function() require'align'.align_to_char(1, true)             end, { desc = "Align to 1 char" }) -- Aligns to 1 character, looking left
-vim.keymap.set('x', '<space>aw', function() require'align'.align_to_char(2, true, true)       end, { desc = "Align to 2 char" }) -- Aligns to 2 characters, looking left and with previews
-vim.keymap.set('x', '<space>as', function() require'align'.align_to_string(false, true, true) end, { desc = "Align to string" }) -- Aligns to a string, looking left and with previews
-vim.keymap.set('x', '<space>ap', function() require'align'.align_to_string(true, true, true)  end, { desc = "Align to pattern" }) -- Aligns to a Lua pattern, looking left and with previews
+vim.keymap.set('x', '<space>as', function()
+    require'align'.align_to_string({
+        preview = true,
+        regex = false,
+    })
+end, { desc = "Align to string" }) -- Aligns to a string, looking left and with previews
+
+vim.keymap.set('x', '<space>aS', function()
+    require'align'.align_to_string({
+        preview = true,
+        regex = true,
+    })
+end, { desc = "Align to string + regex" }) -- Aligns to a string with regex, looking left and with previews
+
+vim.keymap.set('n', '<space>ap', function()
+    require'align'.align_to_operator(
+        require'align'.align_to_string,
+        {
+            regex = false,
+            preview = true,
+        }
+    )
+end, { desc = "Align to string + regex" }) -- Aligns to a paragraph, looking left and with previews
